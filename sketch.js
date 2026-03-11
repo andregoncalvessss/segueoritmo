5dbc29eb3bac2140144d1691d5fa8e86b7108f99
andregoncalvessss <andrefilipegoncalves@ipvc.pt>
2026-03-11 16:34:36 +0000

    menu inicial em desenvolvimento + outros ecras

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 let ecra = 0;
+let imgFundo;
+
+function preload() {
+  imgFundo = loadImage('assets/background.png');
+}
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1280, 720); 
   textAlign(CENTER, CENTER);
 }
 
@@ -16,42 +21,40 @@ function draw() {
 }
 
 function menuPrincipal() {
-  background(255);
-  
-  fill(0);
+  background(imgFundo);
+  fill(255); 
   textSize(60);
-  text("Segue o Ritmo", 400, 150);
-  
-  rect(300, 250, 200, 60);
-  rect(300, 350, 200, 60);
-  
-  fill(255);
+  text("SEGUE O RITMO", 640, 180);
+  fill(220); 
+  rect(540, 300, 200, 60, 10);
+  rect(540, 420, 200, 60, 10); 
+  fill(0);
   textSize(25);
-  text("JOGAR", 400, 280);
-  text("INSTRUÇÕES", 400, 380);
+  text("JOGAR", 640, 330);
+  text("INSTRUÇÕES", 640, 450);
 }
 
 function jogo() {
   background(200);
   fill(0);
   textSize(40);
-  text("O jogo começa aqui!", 400, 300);
+  text("O jogo começa aqui!", 640, 360);
 }
 
 function instrucoes() {
   background(200);
   fill(0);
   textSize(40);
-  text("Como jogar...", 400, 300);
+  text("Como jogar...", 640, 360);
 }
 
 function mousePressed() {
   if (ecra === 0) {
-    if (mouseX > 300 && mouseX < 500 && mouseY > 250 && mouseY < 310) {
+    if (mouseX > 540 && mouseX < 740 && mouseY > 300 && mouseY < 360) {
       ecra = 1;
     }
     
-    if (mouseX > 300 && mouseX < 500 && mouseY > 350 && mouseY < 410) {
+    if (mouseX > 540 && mouseX < 740 && mouseY > 420 && mouseY < 480) {
       ecra = 2;
     }
   } else {
